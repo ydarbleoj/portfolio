f54c9c08acde35d18fbe21b8cc09d66c079bb354
ydarbleoj <brady.joel@gmail.com>
2013-08-05 17:15:09 -0700

    deleted some unnecessary items

--- hoverImage.js
+++
@@ -1,7 +0,0 @@
-$(".imgHover").hover(function(){
-	$(this).children("img").fadeTo(200, 0.25)
-		   .end().children(".hover").show();
-}, function() { 
-	$(this).children("img").fadeTo(200, 1)
-	       .end().children(".hover").hide();
-});
